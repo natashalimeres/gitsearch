758b467251d11f565155ce28e35dbb58224d163c
Natasha Limeres <natashaslimeres@gmail.com>
2024-04-16 14:44:16 -0300

    Revert "Initializing Repository"
    This reverts commit 563dd8505585182e59596ff38f4703e155c696c3.

--- src/graphql/index.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: "https://api.github.com/graphql",
-  headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-  },
-  cache: new InMemoryCache(),
-});
--- src/models/modal.ts
+++
@@ -1,4 +0,0 @@
-export enum EModalActionTypes {
-  openModal = "modal/openModal",
-  closeModal = "modal/closeModal",
-}
